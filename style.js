544b1653be16bfbfe88c0d275b3e14791d6f88fd
AntonGrubkin <elcomendante86@gmail.com>
2022-08-12 00:34:58 +0300

    first commit

---
+++ style.js
@@ -0,0 +1,29 @@
+const burger = document.querySelector('.burger');
+const navMenu = document.querySelector('.nav-menu');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    document.querySelector("body").classList.toggle("scroll-hide");
+    document.querySelector("html").classList.toggle("scroll-hide");
+
+})
+
+document.querySelectorAll(".nav-link").forEach((link) =>
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    navMenu.classList.remove("active");
+    document.querySelector("body").classList.remove("scroll-hide");
+    document.querySelector("html").classList.remove("scroll-hide");
+
+  })
+ 
+);
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 80) {
+        $('.header-nav-container').addClass('fixed');
+    } else {
+        $('.header-nav-container').removeClass('fixed');
+    }
+    });
